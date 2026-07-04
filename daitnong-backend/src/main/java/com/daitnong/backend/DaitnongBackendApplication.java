351844ccf5663fd0ef921bd87bb7f56343268c53
권준형 <wnsgud8845@gmail.com>
2026-07-04 15:47:14 +0900

    init: 초기 세팅

---
+++ daitnong-backend/src/main/java/com/daitnong/backend/DaitnongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daitnong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaitnongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaitnongBackendApplication.class, args);
+    }
+
+}
---
+++ daitnong-backend/src/test/java/com/daitnong/backend/DaitnongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daitnong.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaitnongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
